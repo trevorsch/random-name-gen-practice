043b1f7ceb2eb07f4d44eb9523278c81cfcdad22
Trevor Scheuermann <trevorsch1@gmail.com>
2015-07-13 15:34:57 -0600

    added README

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var students = [
   {name: "test", city: "somewhere"},
   {name: "john", city: "another"},
   {name: "sname", city: "denver"}
-]
+];
 
 var cohort = {
   students : students,
@@ -10,10 +10,10 @@ var cohort = {
   var num = Math.floor(Math.random() * this.students.length);
   console.log(this.students[num].name);
   }
-}
+};
 
 // using no anon functions
-var button = document.getElementsByTagName("button")[0]
+var button = document.getElementsByTagName("button")[0];
 button.addEventListener('click', cohort.pickStudent.bind(cohort));
 
 /*cohort.pickStudent();
