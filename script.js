203390aecf9b53fc5cda8170c5081729939aab29
Trevor Scheuermann <trevorsch1@gmail.com>
2015-07-13 15:32:35 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var students = [
+  {name: "test", city: "somewhere"},
+  {name: "john", city: "another"},
+  {name: "sname", city: "denver"}
+]
+
+var cohort = {
+  students : students,
+  pickStudent : function(){
+  var num = Math.floor(Math.random() * this.students.length);
+  console.log(this.students[num].name);
+  }
+}
+
+// using no anon functions
+var button = document.getElementsByTagName("button")[0]
+button.addEventListener('click', cohort.pickStudent.bind(cohort));
+
+/*cohort.pickStudent();
+cohort.pickStudent();
+cohort.pickStudent();
+cohort.pickStudent();
+cohort.pickStudent();*/
